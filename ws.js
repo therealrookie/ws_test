6e505060849fd867d33da8102d58acd9e222add7
Robin Vollbracht <vollbrachtro84177@th-nuernberg.de>
2025-03-29 16:11:35 +0100

    Deploy #2

--- ws.js
+++ ws.js
@@ -1,5 +1,7 @@
-var WebSocketServer = require("ws").Server,
-  wss = new WebSocketServer({ port: 443 });
+/*
+const ws_port = 433;
+const wss = new WebSocketServer({ port: ws_port });
+console.log('listening for connections on %s...', ws_port);
 
 wss.on("connection", function (ws) {
   ws.on("message", function (message) {
@@ -7,4 +9,16 @@ wss.on("connection", function (ws) {
   });
 
   setInterval(() => ws.send(`${new Date()}`), 5000);
+});
+
+*/
+
+const WebSocket = require("ws");
+const PORT = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port: PORT });
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.log(`Received message => ${message}`);
+  });
+  ws.send("Hello! Message From Server!!");
 });
