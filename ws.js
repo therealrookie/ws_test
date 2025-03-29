d307a2af318e3d2102924bc97444d131a96e8005
Robin Vollbracht <vollbrachtro84177@th-nuernberg.de>
2025-03-29 16:17:28 +0100

    Deploy #3

--- ws.js
+++ ws.js
@@ -14,7 +14,7 @@ wss.on("connection", function (ws) {
 */
 
 const WebSocket = require("ws");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.WS_PORT || 3000;
 const wss = new WebSocket.Server({ port: PORT });
 wss.on("connection", (ws) => {
   ws.on("message", (message) => {
